6d4359e1510634b489aa33f3e36f47e415865532
Zack Bleach <zackb@brandwatch.com>
2015-03-23 14:44:04 -0700

    Run now saves pid

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ -z ${PINGPONG_PORT+x} ]; 
     then 
-        gunicorn app:app -b localhost:6000;
+        gunicorn app:app -b localhost:6000 --pid /tmp/pingpong.pid
     else 
-        gunicorn app:app -b localhost:$PINGPONG_PORT
+        gunicorn app:app -b localhost:$PINGPONG_PORT --pid /tmp/pingpong.pid
 fi
 
